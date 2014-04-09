76708f4ce3349c78a8f8c61d4c2a17c1135cf267
ErikBrändli <elegenstein@student.tgm.ac.at>
2014-04-09 13:46:36 +0200

    Sql_files compatibel mit v0.0.1(Prototyp)

---
+++ ITP.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS itp_proto;
+CREATE DATABASE itp_proto;
+use itp_proto;
+
+CREATE TABLE kunde(
+	id int,
+	vorname VARCHAR(40),
+	nachname VARCHAR(40),
+	adresse VARCHAR(60),
+	debitcard VARCHAR(11),
+	cardcode CHAR(3),
+	primary key (id)
+);
+CREATE TABLE artikel(
+	id int,
+	marke VARCHAR(20),
+	einkaufp int,
+	einkaufdatum date,
+	tageskosten int,
+	verfallzeity int,
+	istSKI boolean,
+	primary key (id)
+);
+drop table if exists abrechnung;
+CREATE TABLE abrechnung(
+	id int,
+	ausgeborgtbis DATE,
+	kid int,
+	aid int,
+	zurueckgegeben BOOLEAN,
+	primary key (id),
+	foreign key (kid) references kunde(id),
+	foreign key (aid) references artikel(id)
+);
